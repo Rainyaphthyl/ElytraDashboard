157381e2e67d537354342749dde522d86033641f
Natfalluvia <amena_rny@outlook.com>
2023-07-11 03:38:45 +0800

    Draft injections

--- src/main/java/io/github/rainyaphthyl/mixin/MixinEntityRenderer.java
+++ src/main/java/io/github/rainyaphthyl/mixin/MixinEntityRenderer.java
@@ -1,18 +1,28 @@
 package io.github.rainyaphthyl.mixin;
 
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.EntityRenderer;
-import org.spongepowered.asm.lib.Opcodes;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(EntityRenderer.class)
 public abstract class MixinEntityRenderer {
+    @Shadow
+    @Final
+    private Minecraft mc;
+
     @Inject(method = "updateCameraAndRender(FJ)V",
-            at = @At(value = "FIELD", target = "Lnet/minecraft/client/settings/GameSettings;smoothCamera:Z", opcode = Opcodes.GETFIELD, shift = At.Shift.BEFORE)
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/client/entity/EntityPlayerSP;turn(FF)V",
+                    shift = At.Shift.NONE)
     )
     private void rotateByKeyboard(float partialTicks, long nanoTime, CallbackInfo ci) {
-        System.out.println("MixinEntityRenderer -> updateCameraAndRender(FJ)V");
+        if (mc.player.isElytraFlying()) {
+            mc.player.turn(-7, 0);
+            System.out.println("MixinEntityRenderer -> updateCameraAndRender(FJ)V");
+        }
     }
 }
