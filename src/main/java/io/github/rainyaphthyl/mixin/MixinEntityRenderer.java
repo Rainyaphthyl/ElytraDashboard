a5c14c649577f36d6ab5b3add10bf60c508bd11a
Natfalluvia <amena_rny@outlook.com>
2023-07-11 15:34:49 +0800

    Update input every tick, rather than every frame (with debug)

--- src/main/java/io/github/rainyaphthyl/mixin/MixinEntityRenderer.java
+++ src/main/java/io/github/rainyaphthyl/mixin/MixinEntityRenderer.java
@@ -4,7 +4,6 @@ import com.mumfrey.liteloader.Tickable;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.EntityRenderer;
 import net.minecraft.client.settings.GameSettings;
-import org.apache.logging.log4j.Logger;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -28,18 +27,23 @@ public abstract class MixinEntityRenderer implements Tickable {
     private static final float elytraDashboard$INERTIA = 0.91F;
     @Shadow
     @Final
-    private static Logger LOGGER;
-    @Shadow
-    @Final
     private Minecraft mc;
+    @Shadow
+    private long prevFrameTime;
     @Unique
-    private float elytraDashboard$yawStrafing = 0.0F;
+    private float elytraDashboard$deltaYaw = 0.0F;
     @Unique
-    private float elytraDashboard$pitchForward = 0.0F;
+    private float elytraDashboard$deltaPitch = 0.0F;
+    @Unique
+    private float elytraDashboard$prevDeltaYaw = 0.0F;
+    @Unique
+    private float elytraDashboard$prevDeltaPitch = 0.0F;
     @Unique
     private float elytraDashboard$prevSensitivity;
     @Unique
     private float elytraDashboard$prevMouseRate;
+    @Unique
+    private float elytraDashboard$prevPartialTick = 0.0F;
 
     {
         elytraDashboard$prevSensitivity = 0.0F;
@@ -52,16 +56,9 @@ public abstract class MixinEntityRenderer implements Tickable {
      */
     @Inject(method = "updateRenderer()V", at = @At("HEAD"))
     private void updateRotationKey(CallbackInfo ci) {
-    }
-
-    /**
-     * Render every frame
-     */
-    @Inject(method = "updateCameraAndRender(FJ)V", at = @At(value = "INVOKE",
-            target = "Lnet/minecraft/client/entity/EntityPlayerSP;turn(FF)V", shift = At.Shift.NONE)
-    )
-    private void rotateByKeyboard(float partialTicks, long nanoTime, CallbackInfo ci) {
         if (mc.player.isElytraFlying()) {
+            elytraDashboard$prevDeltaYaw = elytraDashboard$deltaYaw;
+            elytraDashboard$prevDeltaPitch = elytraDashboard$deltaPitch;
             GameSettings gameSettings = mc.gameSettings;
             float rate;
             float sensitivity = gameSettings.mouseSensitivity;
@@ -88,27 +85,61 @@ public abstract class MixinEntityRenderer implements Tickable {
                 --keyStrafing;
             }
             if (keyStrafing != 0 || keyForward != 0) {
-                elytraDashboard$yawStrafing += (float) keyStrafing * rate;
-                elytraDashboard$pitchForward += (float) keyForward * rate;
-                int i = 1;
-                if (gameSettings.invertMouse) {
-                    i = -1;
-                }
-                mc.player.turn(elytraDashboard$yawStrafing, elytraDashboard$pitchForward * (float) i);
+                elytraDashboard$deltaYaw += (float) keyStrafing * rate;
+                elytraDashboard$deltaPitch += (float) keyForward * rate;
             }
-            if (elytraDashboard$yawStrafing != 0.0F) {
-                elytraDashboard$yawStrafing *= elytraDashboard$INERTIA;
-                if (Math.abs(elytraDashboard$yawStrafing) < 0.003F) {
-                    elytraDashboard$yawStrafing = 0.0F;
+            if (elytraDashboard$deltaYaw != 0.0F) {
+                elytraDashboard$deltaYaw *= elytraDashboard$INERTIA;
+                if (Math.abs(elytraDashboard$deltaYaw) < 0.003F) {
+                    elytraDashboard$deltaYaw = 0.0F;
                 }
             }
-            if (elytraDashboard$pitchForward != 0.0F) {
-                elytraDashboard$pitchForward *= elytraDashboard$INERTIA;
-                if (Math.abs(elytraDashboard$pitchForward) < 0.003F) {
-                    elytraDashboard$pitchForward = 0.0F;
+            if (elytraDashboard$deltaPitch != 0.0F) {
+                elytraDashboard$deltaPitch *= elytraDashboard$INERTIA;
+                if (Math.abs(elytraDashboard$deltaPitch) < 0.003F) {
+                    elytraDashboard$deltaPitch = 0.0F;
                 }
             }
-            LOGGER.info("MixinEntityRenderer -> updateCameraAndRender(FJ)V {}", System.nanoTime());
+            System.err.println("Delta(Rotation): " + elytraDashboard$deltaYaw + " / " + elytraDashboard$deltaPitch);
+        }
+    }
+
+    /**
+     * Render every frame
+     *
+     * @param partialTicks Seems in {@code [0.0F, 1.0F)} ?
+     */
+    @Inject(method = "updateCameraAndRender(FJ)V", at = @At(value = "INVOKE",
+            target = "Lnet/minecraft/client/entity/EntityPlayerSP;turn(FF)V", shift = At.Shift.BEFORE)
+    )
+    private void rotateByKeyboard(float partialTicks, long nanoTime, CallbackInfo ci) {
+        if (mc.player.isElytraFlying()) {
+            int i = 1;
+            if (mc.gameSettings.invertMouse) {
+                i = -1;
+            }
+            float partialDYaw;
+            float partialDPitch;
+            if (elytraDashboard$prevPartialTick < partialTicks) {
+                float currFrame = partialTicks - elytraDashboard$prevPartialTick;
+                partialDYaw = elytraDashboard$deltaYaw * currFrame;
+                partialDPitch = elytraDashboard$deltaPitch * currFrame;
+                System.out.printf("Partial %.4f->%.4f: %+.6f/%+.6f\n", elytraDashboard$prevPartialTick, partialTicks,
+                        partialDYaw, partialDPitch);
+            } else {
+                float priorHalf = 1.0F - elytraDashboard$prevPartialTick;
+                partialDYaw = elytraDashboard$prevDeltaYaw * priorHalf;
+                partialDPitch = elytraDashboard$prevDeltaPitch * priorHalf;
+                System.out.printf("Tail %.4f->%.4f: %+.6f/%+.6f\n", elytraDashboard$prevPartialTick, 1.0F,
+                        partialDYaw, partialDPitch);
+                float postDYaw = elytraDashboard$deltaYaw * partialTicks;
+                float postDPitch = elytraDashboard$deltaPitch * partialTicks;
+                partialDYaw += postDYaw;
+                partialDPitch += postDPitch;
+                System.out.printf("Head %.4f->%.4f: %+.6f/%+.6f\n", 0.0F, partialTicks, postDYaw, postDPitch);
+            }
+            elytraDashboard$prevPartialTick = partialTicks;
+            mc.player.turn(partialDYaw, partialDPitch * (float) i);
         }
     }
 }
