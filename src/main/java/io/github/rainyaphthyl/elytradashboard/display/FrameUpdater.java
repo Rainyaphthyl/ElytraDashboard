5015ab6cb93b5544a5e2fae315485b4d6c06d63b
Natfalluvia <amena_rny@outlook.com>
2023-07-17 12:44:03 +0800

    Remove deprecated javadocs

--- src/main/java/io/github/rainyaphthyl/elytradashboard/display/FrameUpdater.java
+++ src/main/java/io/github/rainyaphthyl/elytradashboard/display/FrameUpdater.java
@@ -1,15 +1,13 @@
 package io.github.rainyaphthyl.elytradashboard.display;
 
-import com.mumfrey.liteloader.client.mixin.MixinMinecraft;
-import io.github.rainyaphthyl.elytradashboard.LiteModElytraDashboard;
-import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.EntityRenderer;
 import net.minecraft.profiler.Profiler;
 
 import javax.annotation.Nonnull;
 
 public interface FrameUpdater {
     /**
-     * Displaying the dashboard data. Called every frame on {@link LiteModElytraDashboard#onTick}
+     * Displaying the dashboard data. Called every frame on {@link EntityRenderer#updateCameraAndRender(float, long)} after rendering In-game GUI and before rendering GUI Screen
      *
      * @param partialTicks Partial tick value within a tick
      * @param inGame       True if in-game, false if in the menu
@@ -17,14 +15,10 @@ public interface FrameUpdater {
     void render(float partialTicks, boolean inGame);
 
     /**
-     * Query dashboard data from the server. Called every tick before {@link FrameUpdater#render} in {@link LiteModElytraDashboard#onTick} at only new ticks.
-     * <p>
-     * This method is mixed into {@link Minecraft#runGameLoop} by {@link MixinMinecraft#onTick}
+     * Query dashboard data from the server. Called every tick before {@link FrameUpdater#render} before {@link EntityRenderer#updateRenderer()}
      *
-     * @param minecraft Minecraft game instance
-     * @param inGame    True if in-game, false if in the menu
+     * @param inGame True if in-game, false if in the menu
      */
-    @SuppressWarnings("JavadocReference")
     void tick(boolean inGame);
 
     @Nonnull
